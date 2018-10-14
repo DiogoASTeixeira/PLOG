869586247f01d5311201ba7b5a289bbd88a6c711
DiogoASTeixeira <diogo_alex@hotmail.com>
2018-10-14 18:37:04 +0100

    First commit
    Initial board created.

---
+++ TP1/zurero.pl
@@ -0,0 +1,17 @@
+create_board(Board) :- 
+	create_board_aux(19, [] , Board).
+
+create_board_aux(0, Board, Board) :- !.
+create_board_aux(N, L, Board) :-
+	create_row(Row),
+	N > 0,
+	N1 is N-1,
+	create_board_aux(N1, [Row|L], Board).
+
+create_row(Row) :- create_row_aux(19, [], Row).
+
+create_row_aux(0, Row, Row) :- !.
+create_row_aux(N, L, Row) :- 
+	N > 0, 
+	N1 is N-1, 
+	create_row_aux(N1, [freeCell|L], Row).
