a303c1926be60f09fe35392a833fc9fef1f19f36
DiogoASTeixeira <diogo_alex9@hotmail.com>
2018-11-08 12:26:23 +0000

    Throwing done!

--- TP1/logic.pl
+++ TP1/logic.pl
@@ -4,7 +4,7 @@ change_player('Black', 'White').
 player_piece('White', 'O').
 player_piece('Black', 'X').
 
-%THROW Direction
+%-------------- THROW Direction ---------------------
 throw_piece(right, Nrow, Piece, BoardIn, BoardOut):-
 	Nrow > 0,
 	Nrow < 20,
@@ -15,26 +15,29 @@ throw_piece(right, Nrow, Piece, BoardIn, BoardOut):-
 			(Piece2 == freeCell,
 				set_piece(2, Nrow, Piece1, BoardIn, Board1),
 				set_piece(1, Nrow, Piece, Board1, BoardOut))).
-throw_piece(up, Ncolumn, Piece, BoardIn, BoardOut):-
+
+throw_piece(down, Ncolumn, Piece, BoardIn, BoardOut):-
 	Ncolumn > 0,
 	Ncolumn < 20,
 	get_piece(Ncolumn, 1, BoardIn, Piece1),
 	get_piece(Ncolumn, 2, BoardIn, Piece2),
 	(Piece1 == freeCell ->
-		throw_piece_up(1, Ncolumn, Piece, BoardIn, BoardOut);
+		throw_piece_down(1, Ncolumn, Piece, BoardIn, BoardOut);
 		(Piece2 == freeCell,
 			set_piece(Ncolumn, 2, Piece1, BoardIn, Board1),
 			set_piece(Ncolumn, 1, Piece, Board1, BoardOut))).
+
 throw_piece(up, Ncolumn, Piece, BoardIn, BoardOut):-
 	Ncolumn > 0,
 	Ncolumn < 20,
 	get_piece(Ncolumn, 19, BoardIn, Piece1),
 	get_piece(Ncolumn, 18, BoardIn, Piece2),
 	(Piece1 == freeCell ->
-		throw_piece_up(1, Ncolumn, Piece, BoardIn, BoardOut);
+		throw_piece_up(19, Ncolumn, Piece, BoardIn, BoardOut);
 		(Piece2 == freeCell,
 			set_piece(Ncolumn, 18, Piece1, BoardIn, Board1),
 			set_piece(Ncolumn, 19, Piece, Board1, BoardOut))).
+		
 throw_piece(left, Nrow, Piece, BoardIn, BoardOut):-
 	Nrow > 0,
 	Nrow < 20,
@@ -46,17 +49,9 @@ throw_piece(left, Nrow, Piece, BoardIn, BoardOut):-
 			set_piece(18, Nrow, Piece1, BoardIn, Board1),
 			set_piece(19, Nrow, Piece, Board1, BoardOut))).
 
-throw_piece(up, Ncolumn, Piece, BoardIn, BoardOut):-
-	Ncolumn > 0,
-	Ncolumn < 20,
-	get_piece(Ncolumn, 19, BoardIn, Piece1),
-	get_piece(Ncolumn, 18, BoardIn, Piece2),
-	(Piece1 == freeCell ->
-		throw_piece_up(1, Ncolumn, Piece, BoardIn, BoardOut);
-		(Piece2 == freeCell,
-			set_piece(Ncolumn, 18, Piece1, BoardIn, Board1),
-			set_piece(Ncolumn, 19, Piece, Board1, BoardOut))).
-		
+throw_piece(_, _, _, BoardIn, BoardIn):-
+	write('Invalid move!'),nl,!.
+%---------END THROW Direction-----------%
 
 throw_piece_right(N, Nrow, Piece, BoardIn, BoardOut):-
 	get_piece(N, Nrow, BoardIn, NPiece),
@@ -72,6 +67,7 @@ throw_piece_right(N, Nrow, Piece, BoardIn, BoardOut):-
 	NN is N + 1,
 	throw_piece_right(NN, Nrow, Piece, BoardIn, BoardOut).
 
+%--------------------------------%
 throw_piece_left(N, Nrow, Piece, BoardIn, BoardOut):-
 	get_piece(N, Nrow, BoardIn, NPiece),
 	NPiece \= freeCell,
@@ -86,6 +82,31 @@ throw_piece_left(N, Nrow, Piece, BoardIn, BoardOut):-
 	NN is N - 1,
 	throw_piece_left(NN, Nrow, Piece, BoardIn, BoardOut).
 
-%throw_piece(left, N, Piece).
-%throw_piece(up, N, Piece).
-%throw_piece(down, N, Piece).
+throw_piece_up(N, Ncolumn, Piece, BoardIn, BoardOut):-
+	get_piece(Ncolumn, N, BoardIn, NPiece),
+	NPiece \= freeCell,
+	N < 19,
+	Nr is N + 1,
+	set_piece(Ncolumn, Nr, Piece, BoardIn, Board1),
+	move_piece(up, Ncolumn, N, Board1, Board2),
+	move_piece(up, Ncolumn, Nr, Board2, BoardOut).
+throw_piece_up(N, Ncolumn, Piece, BoardIn, BoardOut):-
+	get_piece(Ncolumn, N, BoardIn, NPiece),
+	NPiece == freeCell,
+	NN is N - 1,
+	throw_piece_up(NN, Ncolumn, Piece, BoardIn, BoardOut).
+
+throw_piece_down(N, Ncolumn, Piece, BoardIn, BoardOut):-
+	get_piece(Ncolumn, N, BoardIn, NPiece),
+	NPiece \= freeCell,
+	N > 1 ,
+	Nr is N - 1,
+	set_piece(Ncolumn, Nr, Piece, BoardIn, Board1),
+	move_piece(down, Ncolumn, N, Board1, Board2),
+	move_piece(down, Ncolumn, Nr, Board2, BoardOut).
+throw_piece_down(N, Ncolumn, Piece, BoardIn, BoardOut):-
+	get_piece(Ncolumn, N, BoardIn, NPiece),
+	NPiece == freeCell,
+	NN is N + 1,
+	throw_piece_down(NN, Ncolumn, Piece, BoardIn, BoardOut).
+
--- TP1/zurero.pl
+++ TP1/zurero.pl
@@ -8,11 +8,11 @@ play:-
 	create_board(Board),
 	print_board(Board),
 	%set_piece(1,2,'X', Board, BoardM),
-	 set_piece(1,1,'X', Board, BoardJ),
+	 set_piece(2,19,'X', Board, BoardJ),
 	%set_piece(2,4,'X', BoardG, BoardJ),^
 
 	print_board(BoardJ),
-	throw_piece(left, 1, 'O', BoardJ, Board1),
+	throw_piece(down, 2, 'O', BoardJ, Board1),
 	print_board(Board1).
 	%play_aux('Black',Board).
 
