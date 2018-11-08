18468b24a5885fb401d560e29b3df6d0e4d78ebf
DiogoASTeixeira <diogo_alex9@hotmail.com>
2018-11-08 19:14:01 +0000

    Game Loop WIP

--- TP1/zurero.pl
+++ TP1/zurero.pl
@@ -4,7 +4,25 @@
 :- include('logic.pl').
 :- include('board.pl').
 
-play:-
+process_direction('right', right):-
+	write('Throwing Right').
+process_direction('left', left):-
+	write('Throwing Left').
+process_direction('up', up):-
+	write('Throwing Up').
+process_direction('down', down):-
+	write('Throwing Down').
+process_direction(_, invalid):-
+	write('Invalid direction'), fail.
+
+%quick tests
+ex:- 
+	read(X),
+	X == 1,
+	write(X).
+
+%bigger tests
+test:-
 	create_board(Board),
 	print_board(Board),
 	set_piece(10,10,'O', Board, Board1),
@@ -15,8 +33,31 @@ play:-
 
 	print_board(Board5),!,
 	check_win(white, Board5).
-		%play_aux('Black',Board).
 
-%play_aux(Player, Board):-
-%	player_piece(Player, Piece).
+game:-
+	create_board(Board),!,
+	loop(white, Board).
 
+loop(Player, Board):-
+	write(Player),
+	write(' it\'s your turn.\n Write the direction (left, right, up, down) that you want to throw: '),
+	read(Input1),
+	(process_direction(Input1, Direction) ->
+		nl,
+		write('Now write the line or column number: '),
+		read(Input2),
+		player_piece(Player, Piece),
+		(throw_piece(Direction, Input2, Piece, Board, NewBoard) ->
+			
+		change_player(Player, Opponent),
+		(check_win(Player, Board) ->
+			fail;
+			(check_win(Opponent, Board) ->
+				fail;
+				loop(Player, NewBoard)
+				
+				)	
+
+
+	;	%ELSE of process_direction
+		!,loop(Player, Board)).
