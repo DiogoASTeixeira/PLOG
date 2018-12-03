da8dfb46e454c1cc889c11085f19f05ac82ffb83
DiogoASTeixeira <diogo_alex9@hotmail.com>
2018-12-03 16:17:40 +0000

    That typo from the presentation

--- TP1/AI.pl
+++ TP1/AI.pl
@@ -30,13 +30,13 @@ get_winning_play(Board, _, VList, Piece, NWin, Direction):-
 get_winning_hor(Board, [X|Tail], Piece, X, right):-
 	X =< Tail,
 	player_piece(Player, Piece),
-	throw_piece(right, X, Piece, Board, BoardOut),
+	move(right, X, Piece, Board, BoardOut),
 	check_win(Player, BoardOut).
 
 get_winning_hor(Board, [X|Tail], Piece, X, left):-
 	X =< Tail,
 	player_piece(Player, Piece),
-	throw_piece(left, X, Piece, Board, BoardOut),
+	move(left, X, Piece, Board, BoardOut),
 	check_win(Player, BoardOut).
 
 get_winning_hor(Board, [X|Tail], Piece, NWin, Direction):-
@@ -47,13 +47,13 @@ get_winning_hor(Board, [X|Tail], Piece, NWin, Direction):-
 get_winning_vert(Board, [X|Tail], Piece, X, up):-
 	X =< Tail,
 	player_piece(Player, Piece),
-	throw_piece(up, X, Piece, Board, BoardOut),
+	move(up, X, Piece, Board, BoardOut),
 	check_win(Player, BoardOut).
 
 get_winning_vert(Board, [X|Tail], Piece, X, down):-
 	X =< Tail,
 	player_piece(Player, Piece),
-	throw_piece(down, X, Piece, Board, BoardOut),
+	move(down, X, Piece, Board, BoardOut),
 	check_win(Player, BoardOut).
 
 get_winning_vert(Board, [X|Tail], Piece, NWin, Direction):-
@@ -139,27 +139,27 @@ valid_moves(Board, HList, VList, Piece):-
 	valid_vert(Board, VList, 1, Piece).
 
 valid_hor(Board, [N|HList], N, Piece):-
-	throw_piece(right, N, Piece, Board, _),
+	move(right, N, Piece, Board, _),
 	valid_hor_last(Board, HList, 19, Piece).
 valid_hor(Board, HList, N, Piece):-
 	NN is N+1,
 	valid_hor(Board, HList, NN, Piece).
 
 valid_hor_last(Board, [N|[]], N, Piece):-
-	throw_piece(right, N, Piece, Board, _).
+	move(right, N, Piece, Board, _).
 valid_hor_last(Board, HList, N, Piece):-
 	NN is N-1,
 	valid_hor_last(Board, HList, NN, Piece).
 
 valid_vert(Board, [N|VList], N, Piece):-
-	throw_piece(up, N, Piece, Board, _),
+	move(up, N, Piece, Board, _),
 	valid_vert_last(Board, VList, 19, Piece).
 valid_vert(Board, VList, N, Piece):-
 	NN is N+1,
 	valid_vert(Board, VList, NN, Piece).
 
 valid_vert_last(Board, [N|[]], N, Piece):-
-	throw_piece(up, N, Piece, Board, _).
+	move(up, N, Piece, Board, _).
 valid_vert_last(Board, VList, N, Piece):-
 	NN is N-1,
 	valid_vert_last(Board, VList, NN, Piece).
