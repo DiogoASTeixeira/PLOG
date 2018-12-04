7e75acdd8f1a129ceca5db9a59163445c84c83b9
DiogoASTeixeira <diogo_alex9@hotmail.com>
2018-12-04 14:56:24 +0000

    It kinda ssolves it

--- TP2/lol.pl
+++ TP2/lol.pl
@@ -2,24 +2,38 @@
 :- use_module(library(lists)).
 :- use_module(library(clpfd)).
 
-sudoku(Puzzle) :-
-	flatten(Puzzle, Tmp),
-	length(Puzzle, Length),
+puzzle(Puzzle):-
+    Puzzle = [
+        [_,_,4,1],
+        [_,_,_,3],
+        [_,_,_,_],
+        [2,_,_,_]
+    ].
+
+sudoku:-
+    puzzle(Puzzle),
+    Rows = [A,B,C,D],
+    write(Puzzle),nl,
+    length(Puzzle, Length),
+    flat(Puzzle, Tmp),
 	domain(Tmp, 1, Length),
 	Rows = Puzzle,
     transpose(Rows, Columns),
-    write(Rows),
-    write(Columns),
+    write(Rows),nl,
+    write(Columns),nl,
     % blocks(Rows, Blocks),
     maplist(all_distinct, Rows),
     maplist(all_distinct, Columns),
     % maplist(all_distinct, Blocks),
-    labeling([], Rows).
+    label_all(Rows),
+    write(Rows).
 
-flatten([], []) :- !.
-flatten([L|Ls], FlatL) :-
-    !,
-    flatten(L, NewL),
-    flatten(Ls, NewLs),
-    append(NewL, NewLs, FlatL).
-flatten(L, [L]).
+label_all([]).
+label_all([H|T]):-
+    labeling([],H),
+    label_all(T).
+
+flat([],[]).
+flat([H|T], R):-
+    flat(T,TR),
+    append(H,TR,R).
