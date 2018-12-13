24ddb220a915cd6be2286a57bd3b7848ea9e749e
DiogoASTeixeira <diogo_alex9@hotmail.com>
2018-12-13 13:28:49 +0000

    Wrote alot, then erased, then wrote alot

--- TP2/lol.pl
+++ TP2/lol.pl
@@ -2,19 +2,30 @@
 :- use_module(library(lists)).
 :- use_module(library(clpfd)).
 
-puzzle(Puzzle):-
+puzzle(p1,Puzzle):-
     Puzzle = [
         [_,_,4,1],
         [_,_,_,3],
         [_,_,_,_],
         [2,_,_,_]
     ].
+map_sudoku(p1, Map) :-
+    Map = [
+        [_,_,_,_],
+        [1,1,1,_],
+        [_,1,_,_],
+        [_,_,_,_]
+    ].
 
 sudoku(Rows):-
-    puzzle(Puzzle),
-    Rows = [A,B,C,D],
-    write(Puzzle),nl,
+    puzzle(p1,Puzzle),
+    map_sudoku(p1, Map),
     length(Puzzle, Length),
+
+    create_global_list(Length, GlobalList),
+    flat(Map, FlatMap),
+    global_cardinality(FlatMap, GlobalList),
+
     flat(Puzzle, Tmp),
 	domain(Tmp, 1, Length),
 	Rows = Puzzle,
@@ -37,3 +48,30 @@ flat([],[]).
 flat([H|T], R):-
     flat(T,TR),
     append(H,TR,R).
+
+create_global_list(Length, List):-
+    create_global_list_aux(1, Length, List).
+
+create_global_list_aux(Length, Length, [Length-Length|[]]).
+create_global_list_aux(N, Length, [N-Length|T]):-
+    NN is N+1,
+    create_global_list_aux(NN, Length, T).
+ 
+get_member(X, Y, List, Value):-
+	get_row(Y, List, L),
+	get_column(X, L, Value).
+	
+get_row(1,[L|_], L).
+get_row(Y, [_|Tail], L):-
+	YY is Y -1,
+	get_row(YY, Tail, L).
+
+get_column(1, [Value|_], Value).
+get_column(X, [_|Tail], Value):-
+	XX is X-1,
+	get_column(XX, Tail, Value).
+
+
+adjacent(Map, Length) :-
+    adj_aux(1, Map, Length).
+
